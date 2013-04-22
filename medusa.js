1452835f6946312e9aece0c0fe8ad17523666d0a
Arnaud Soyez <weboide@codealpha.net>
2013-04-21 20:54:38 -0400

    Added core, config modules.
    Implemented simplistic core and config modules. The config modules has a set of default settings and can load a JSON file as config file to override the default settings.
    In medusa.js, if --config is given core.js will load the given config file.

--- medusa.js
+++ medusa.js
@@ -1,25 +1,41 @@
 #!/usr/bin/env node
 var optimist = require('optimist');
+var core = require('./lib/core');
 var argv = optimist
-    .usage('Lightweight distributed system monitoring tool powered by node.js.\nUsage: $0 [start|stop|status|cli]')
+    .usage('Lightweight distributed system monitoring tool powered by node.js.\n\nUsage: $0 [start|stop|status|cli]')
     .options('h', {
         alias: 'help',
         description: 'Show this help message'
+    })
+    .options('V', {
+        alias: 'version',
+        description: 'Print the version number',
+        boolean: true
+    })
+    .options('config', {
+        description: 'Specify a configuration file to use instead of the default one.',
+        string: true
     })
     .argv;
 
 // parsing args
 
-// console.log('optargs are:');
-// console.dir(argv);
+if(argv.config)
+{
+    core.loadConfig(argv.config);
+}
 
-if(argv._.length == 0 || argv.help || argv.h)
+if(argv.help || argv.h) // show help
 {
     optimist.showHelp();
 }
 else if(argv._.length && ['start','stop','status','cli'].indexOf(argv._[0]) >= 0) // see if the first non-hyphenated option is either start/stop/status/cli
 {
     console.log('action: '+argv._[0]);
+}
+else if(argv.V || argv.version) // show version
+{
+    console.log('medusa-monitoring v'+core.version);
 }
 
 
