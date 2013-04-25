dffe1f00bc8dd71477f8c1074391bc194dc6ca94
Arnaud Soyez <weboide@codealpha.net>
2013-04-25 18:21:34 -0400

    Added uncommitted changes to the Httpservice.

--- modules/http/services/http.js
+++ modules/http/services/http.js
@@ -10,38 +10,9 @@ var HttpService = module.exports = function HttpService() {
 }
 util.inherits(HttpService, BaseService)
 
-/**
- * Sends a ping to the specified address, and callbacks when done. (async)
- */
-HttpService.prototype.probe = function(addr, cb) {
-        var p = os.platform();
-        var ls = null;
-
-
-        if (p == 'linux') {
-            //linux
-            ls = cp.spawn('/bin/ping', ['-n', '-w 2', '-c 1', addr])
-        } else if (p.match(/^win/)) {
-            //windows
-            ls = spawn('C:/windows/system32/ping.exe', ['-n', '1', '-w', '5000', addr])
-        } else if (p == 'darwin') {
-            //mac os
-            ls = spawn('/sbin/ping', ['-n', '-t 2', '-c 1', addr])
-        }
-
-        ls.on('error', function(e) {
-            cb && cb(new Error('There was an error while executing the ping program.'))
-            // TODO: maybe handle an error callback?
-        });
-
-        ls.on('exit', function (code) {
-            var result = (code === 0 ? true : false)
-            cb && cb(null, result)
-        });
-}
 
 /**
- * Sends a ping to the machine, and callbacks when done. (async)
+ * Sends a http request to the machine, and callbacks when done. (async)
  */
 HttpService.prototype.check = function(machine, cb) {
     console.log('FAKE HTTP ALIVE (TODO)') // TODO: fix this
