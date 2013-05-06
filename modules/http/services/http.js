851f9c5c893d588348fe3718f10290f01a0162df
Arnaud Soyez <weboide@codealpha.net>
2013-05-05 21:56:46 -0400

    Implemented HTTP Service's basic GET request.

--- modules/http/services/http.js
+++ modules/http/services/http.js
@@ -5,6 +5,8 @@ var cp = require('child_process')
     , os = require('os')
     , timers = require('timers')
     , util = require('util')
+    , http = require('http')
+    , async = require('async')
 
 var HttpService = module.exports = function HttpService(machine, options) {
     HttpService.super_.apply(this, arguments) // call parent constructor
@@ -23,10 +25,29 @@ HttpService.prototype.startServiceCheck = function(cb) {
 }
 
 /**
+ * Checks if the URL is accessible and calls the callback.
+ * callback is called with cb(accessible, status_code, response_time)
+ */
+HttpService.prototype.checkURL = function(url, cb) {
+    http.get(url, function(res) {
+      console.log("URL '"+url+"' returned: " + res.statusCode);
+      cb(res.statusCode == 200, res.statusCode, 0)
+    }).on('error', function(e) {
+      console.log("Got error: " + e.message);
+      cb(false, -1, 0)
+    });
+}
+
+/**
  * Sends a http request to the machine, and callbacks when done. (async)
  */
 HttpService.prototype.check = function(cb) {
-    console.log('FAKE HTTP ALIVE (TODO) for machine: '+this.machine.config.name) // TODO: fix this
+    async.every(this.options.urls, this.checkURL, function(result){
+        if(result)
+            console.log('Machine "'+this.machine.config.name+'" is responsive for http service.')
+        else
+            console.log('Machine "'+this.machine.config.name+'" is dead for http service. One of the URLs failed.')
+    }.bind(this))
     this.emit('service:check:alive')
     cb && cb()
 }
